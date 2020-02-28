f72df8a47604b4940310f609d60cbf48343b372b
Darkonnen <adel.cabrera.mesina@gmail.com>
2020-02-28 14:47:23 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+  for(int i = 0; i <= 5; i++){
+	  print('Hello World! $i');
+    }
+
+  String nombre;
+  nombre = "Adel";
+  print("Mi nombre es $nombre");
+  print(nombre[0]);
+
+
+  int empleados;
+  empleados = 10;
+  
+  double pi;
+  pi = 3.1415;
+
+  print("$empleados - $pi");
+
+}
